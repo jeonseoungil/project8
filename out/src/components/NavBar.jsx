e192625b4563261a63903ecdfb11d3ff61dbe6dd
jsi <lyola2511@naver.com>
2023-08-04 11:17:15 +0900

    add to file

--- out/src/components/NavBar.jsx
+++ out/src/components/NavBar.jsx
@@ -1,7 +1,15 @@
 import React, { memo, useEffect, useState } from "react";
 import { NavBardiv } from "../assets/css/MusicSub";
 import { Link, NavLink, useNavigate } from "react-router-dom";
-import { AiFillBell, AiFillDropboxCircle, AiFillGift, AiFillHome, AiFillTrophy, AiOutlineLogout, AiOutlineUser } from "react-icons/ai";
+import {
+  AiFillBell,
+  AiFillDropboxCircle,
+  AiFillGift,
+  AiFillHome,
+  AiFillTrophy,
+  AiOutlineLogout,
+  AiOutlineUser,
+} from "react-icons/ai";
 import { useDispatch, useSelector } from "react-redux";
 import { LOGOUT } from "../store/modules/UserSlice";
 import { loggout } from "../store/modules/LoginSignUp";
@@ -25,13 +33,12 @@ const NavBar = memo(() => {
     navi("/");
   };
 
-    const getInitialSize = () => {
-      return window.innerWidth <= 1400 ? 2 : 3;
-    };
-  
-    const [size, setsize] = useState(getInitialSize());
+  const getInitialSize = () => {
+    return window.innerWidth <= 1400 ? 2 : 3;
+  };
 
-  
+  const [size, setsize] = useState(getInitialSize());
+
   useEffect(() => {
     const handleResize = () => {
       const width = window.innerWidth;
@@ -55,38 +62,54 @@ const NavBar = memo(() => {
   };
   return (
     <NavBardiv>
-      <h2><NavLink to="/" >{size ===3 ? <img className="big" src="./biglogo.png" alt="" />
-      : <img className="small" src="/vite.svg" alt="" /> }</NavLink></h2>
+      <h2>
+        <NavLink to="/">
+          {size === 3 ? (
+            <img className="big" src="/biglogo.png" alt="" />
+          ) : (
+            <img className="small" src="/vite.svg" alt="" />
+          )}
+        </NavLink>
+      </h2>
       <hr />
       {user ? (
         <>
-        { size===3? <Link to={`/storage`}>
-          <span> {login_UserID} 님 환영합니다!  </span>
-        </Link> : null}
-        <button className="logoutbit" onClick={() => savelogout()}> <AiOutlineLogout/>{size===3?"로그아웃":""}</button>
+          {size === 3 ? (
+            <Link to={`/storage`}>
+              <span> {login_UserID} 님 환영합니다! </span>
+            </Link>
+          ) : null}
+          <button className="logoutbit" onClick={() => savelogout()}>
+            {" "}
+            <AiOutlineLogout />
+            {size === 3 ? "로그아웃" : ""}
+          </button>
         </>
       ) : (
         <NavLink to={`/Login`}>
           <AiOutlineUser />
-          <span>{size===3?"로그인":""}</span>{" "}
+          <span>{size === 3 ? "로그인" : ""}</span>{" "}
         </NavLink>
       )}
       <hr />
-      {size===3? <><form onSubmit={searchsubmit}>
-        <input
-          type="text"
-          placeholder="검색 가능"
-          value={searchtext}
-          onChange={inputchange}
-        />
-      </form>
-      <hr />
-      </>
-      :null}
+      {size === 3 ? (
+        <>
+          <form onSubmit={searchsubmit}>
+            <input
+              type="text"
+              placeholder="검색 가능"
+              value={searchtext}
+              onChange={inputchange}
+            />
+          </form>
+          <hr />
+        </>
+      ) : null}
       <ul>
         <li>
           <NavLink to="/" className={({ isActive }) => (isActive ? "on" : "")}>
-          <AiFillHome/>{size===3?"홈":""}
+            <AiFillHome />
+            {size === 3 ? "홈" : ""}
           </NavLink>
         </li>
         <li>
@@ -94,7 +117,7 @@ const NavBar = memo(() => {
             to="/chart"
             className={({ isActive }) => (isActive ? "on" : "")}
           >
-            <AiFillTrophy /> {size===3?"차트":""}
+            <AiFillTrophy /> {size === 3 ? "차트" : ""}
           </NavLink>
         </li>
         <li>
@@ -103,7 +126,8 @@ const NavBar = memo(() => {
             onClick={box}
             className={({ isActive }) => (isActive ? "on" : "")}
           >
-            <AiFillDropboxCircle/>{size===3?"보관함":""}
+            <AiFillDropboxCircle />
+            {size === 3 ? "보관함" : ""}
           </NavLink>
         </li>
         <li>
@@ -111,7 +135,8 @@ const NavBar = memo(() => {
             to="/list"
             className={({ isActive }) => (isActive ? "on" : "")}
           >
-            <AiFillBell/>{size===3?"리스트":""}
+            <AiFillBell />
+            {size === 3 ? "리스트" : ""}
           </NavLink>
         </li>
         <li>
@@ -119,7 +144,8 @@ const NavBar = memo(() => {
             to="/event"
             className={({ isActive }) => (isActive ? "on" : "")}
           >
-            <AiFillGift/>{size===3?"멤버쉽":""}
+            <AiFillGift />
+            {size === 3 ? "멤버쉽" : ""}
           </NavLink>
         </li>
       </ul>
